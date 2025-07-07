2ed0d7d587d68fd525991e51f235c3ed1d29af62
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2025-07-07 11:25:32 +0300

    added functionality that check if a room exists or not, a player exists or not, moving and rotating the current player

--- Lib.cs
+++ Lib.cs
@@ -11,11 +11,12 @@ public static partial class Module
         [PrimaryKey]
         [AutoInc]
         public int RoomID;
-        public uint PlayersInRoom;
+        public byte PlayersInRoom;
         public bool GameStarted;
         public Identity CurrentPlayerTurn;
         public Identity[] Players = [];
         public int TimeLeft;
+       public byte ActionsRemained;
 
 
     }
@@ -29,8 +30,8 @@ public static partial class Module
         public bool Online;
         public int HP;
 
-        public float[] Location = [0, 0, 0];
-        public float[] Rotation = [0, 0, 0];
+        public int[] Location = [0, 0, 0];
+        public byte Rotation; // reflects the number by 90 degress basically Rotation * 90 
 
     }
 
@@ -76,6 +77,8 @@ public static partial class Module
     }
 
 
+
+
     [Reducer]
     public static void PlayerChangedName(ReducerContext ctx, string NewName)
     {
@@ -93,9 +96,9 @@ public static partial class Module
 
 
     [Reducer]
-    public static void JoinRoom(ReducerContext ctx,int roomID)
+    public static void JoinRoom(ReducerContext ctx, int roomID)
     {
-        
+
         var player = ctx.Db.Player.identity.Find(ctx.Sender);
         var room = ctx.Db.Room.RoomID.Find(roomID);
 
@@ -126,11 +129,89 @@ public static partial class Module
 
                 ctx.Db.Room.RoomID.Update(room);
 
-                
-                
+
+
             }
 
         }
 
     }
+
+
+    [Reducer] 
+    public static void MoveRotateAttack(ReducerContext ctx, int roomID, byte action,  int[] direction)
+    {
+
+
+        // check if the player and room exist
+        var room = RoomExist(roomID, ctx);
+        var player = PlayerExist(ctx);
+
+        if (room is not null && player is not null)
+        {
+            //check if it's the current player turn
+
+            //check if the player has more actions to do
+            if (room.CurrentPlayerTurn == player.identity && room.ActionsRemained > 0)
+            {
+                // reduce the actions that remains
+                room.ActionsRemained--;
+
+
+                switch (action)
+                {
+
+                    case 0: // move in the given direction
+                     var newPosition = new int[3];
+                    for (int i = 0; i < 3; i++)
+                        {
+                            newPosition[i] = player.Location[i] + Math.Clamp(direction[i], -1, 1);
+                        }
+
+                        player.Location = newPosition;
+                        break;
+                    case 1: // rotate
+
+                    player.Rotation +=(byte) direction[0];
+                    player.Rotation %= 4;
+                        break;
+                    case 2: // attack
+                            //check all the players and see if any of them exists in that place
+                            //if so then inflict damage on that player
+                    
+                    
+                        break;
+                    case 3: // do nothing
+                        break;
+                }
+
+                //
+
+            }
+
+        }
+
+
+
+
+    }
+
+
+
+    private static Room? RoomExist(int roomID, ReducerContext context)
+    {
+
+
+        return context.Db.Room.RoomID.Find(roomID) ;
+    }
+
+    private static Player? PlayerExist( ReducerContext context)
+    {
+
+
+        return context.Db.Player.identity.Find(context.Sender) ;
+    }
+    
+
+
 }
